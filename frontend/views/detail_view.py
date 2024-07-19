e048764479b8ad022a791d04648c1a6daea4d64f
Roni ahmadi <ronironiahmadi@gmail.com>
2024-07-20 06:25:59 +0700

    update frontend

--- frontend/views/detail_view.py
+++ frontend/views/detail_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-from produk.models import GambarProduk, Produk
+from produk.models import GambarProduk, Produk, UlasanCart
 from profiles.models import LangSupport
 
 from .base_view import FrontPage
@@ -15,8 +16,13 @@ class Detail(FrontPage):
             gambar = GambarProduk.objects.filter(produk_id=produk.id)
         except Exception as e:
             print(e)
+
+        ulasan = UlasanCart.objects.filter(produkitem_id=produk.id)
+        ulasan = Paginator(ulasan, 10)
+        ulasan_page_num = request.GET.get("page", 1)
+        ulasan = ulasan.page(ulasan_page_num)
         return render(
             request,
             "home/detail.html",
-            {"slug": slug, "produk": produk, "gambar": gambar, "languages": languages},
+            {"slug": slug, "produk": produk, "gambar": gambar, "languages": languages, "ulasan": ulasan},
         )
