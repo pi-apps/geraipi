3467f965023f06be9d6dc8692ddc5907d6ad53aa
Ahmadi Roni <roniahmadi30@gmail.com>
2024-09-04 22:05:02 +0700

    update

--- projekpi/settings.py
+++ projekpi/settings.py
@@ -169,12 +169,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.getenv("STATIC_PATH", os.path.join(BASE_DIR, "static"))
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 STATIC_URL = "static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.getenv("MEDIA_PATH", os.path.join(BASE_DIR, "media"))
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
 # Default primary key field type
